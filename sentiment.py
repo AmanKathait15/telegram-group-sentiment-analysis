9ef90fbbefb06052d52b7a155a02f215d07c90c8
Aman Kathait <agnikathait171@gmail.com>
2020-12-17 20:06:10 +0530

    dec17

---
+++ sentiment.py
@@ -0,0 +1,256 @@
+
+import telebot , re
+from textblob import TextBlob 
+from googletrans import Translator
+from config import *
+
+from matplotlib import pyplot as plt
+import matplotlib.animation as animation
+from random import random,randint
+
+from time import localtime , strftime
+
+
+bot = telebot.TeleBot(bot_token)
+
+translator_flag = False
+
+sentiment_flag  = False
+
+calculator_flag = False
+
+index = 0
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+
+	bot.reply_to(message, "Welcome to Sentiment bot enter text and this bot will tell the Sentiment of enter text :)")
+
+@bot.message_handler(commands=['OnLiveSentiemnt', 'ols'])
+def show_sentiment(message):
+
+	global sentiment_flag
+
+	sentiment_flag = True
+
+	bot.reply_to(message, "from now on all message live sentiment score will reply by bot \n to off this use /OffLiveSentiment or /OLS command")
+
+@bot.message_handler(commands=['OffLiveSentiemnt', 'OLS'])
+def hide_sentiment(message):
+
+	global sentiment_flag
+
+	sentiment_flag = False
+
+	bot.reply_to(message, "Successfully off Live Sentiemnt")
+
+@bot.message_handler(commands=['OnCalculator', 'oc'])
+def on_calculator(message):
+
+	global calculator_flag
+
+	calculator_flag = True
+
+	bot.reply_to(message, "try expression like (a*b)+(c/d) and bot will give answer \n to off this use /OffCalculator or /OC command")
+
+@bot.message_handler(commands=['OffLiveSentiemnt', 'OC'])
+def off_calculator(message):
+
+	global calculator_flag
+
+	calculator_flag = False
+
+	bot.reply_to(message, "Successfully off Calculator")
+
+@bot.message_handler(commands=['OnTranslator', 'ot'])
+def on_Translator(message):
+
+	global translator_flag
+
+	translator_flag = True
+
+	bot.reply_to(message, "now all message which are not type in english ar translated to english \n to off this use /OffCalculator or /OC command")
+
+@bot.message_handler(commands=['OffTranslator', 'OT'])
+def off_Translator(message):
+
+	global translator_flag
+
+	translator_flag = False
+
+	bot.reply_to(message, "Successfully off Translator")
+
+
+@bot.message_handler(commands=['off'])
+def turnON(message):
+
+	global sentiment_flag,calculator_flag,translator_flag
+
+	calculator_flag = False
+
+	translator_flag = False
+
+	sentiment_flag  = False
+
+	bot.reply_to(message, "All active commands are turn off")
+
+@bot.message_handler(commands=['on'])
+def turnOFF(message):
+
+	global sentiment_flag,calculator_flag,translator_flag
+
+	calculator_flag = True
+
+	translator_flag = True
+
+	sentiment_flag  = True
+
+	bot.reply_to(message, "All commands are turn on")
+
+@bot.message_handler(commands=['help','status'])
+def show_status(message):
+
+	output ="All commands :\
+			 \n /start\
+			 \n /help\
+			 \n /status\
+			 \n /on\
+			 \n /off\
+			 \n /OnCalculator or /oc\
+			 \n /OffCalculator or /OC\
+			 \n /OnLiveSentiemnt or /ols\
+			 \n /OffLiveSentiemnt or /OLS\
+			 \n /OnTranslator or /ot\
+			 \n /OffTranslator or /Ot\
+			 \n\nStatus : \
+			 \nLive Sentiemnt : "+str(sentiment_flag)+"\
+			 \nCalculator : "+str(calculator_flag)+"\
+			 \nTranslator : "+str(translator_flag)
+
+	bot.reply_to(message,output)
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+
+	#print(type(message))
+
+	global index
+
+	print()
+
+	index+=1
+
+	try:
+		msg_id = message.message_id
+
+		date_time = strftime("%d/%m/%Y %H:%M:%S", localtime())
+
+		print("message_id : "+str(msg_id))
+
+		fn = message.from_user.first_name
+
+		ln = message.from_user.last_name
+
+		if(ln!=None):
+
+			name = fn+" "+ln
+		
+		else:
+			
+			name = fn
+
+		print("user name : "+ name)
+
+		orig_msg = message.text
+
+		print("original message : "+orig_msg)
+
+		translator = Translator(service_urls=['translate.googleapis.com'])
+
+		trans_msg = translator.translate(orig_msg).text
+
+		print("translated message : "+trans_msg)
+
+		trans_msg = re.sub(r'[,]',' ',trans_msg)
+
+		if(translator_flag and trans_msg!=orig_msg):
+
+			bot.reply_to(message,trans_msg)
+
+		if(calculator_flag):
+
+			try:
+				
+				#exp = str(eval(exp))
+
+				exp = re.sub(r'[=]','==',trans_msg)
+
+				exp = re.sub(r'[\^]','**',exp)
+
+				exp = re.sub(r'[^0-9\*\(\)-+/%]','',exp)
+
+				if(len(exp)>2):
+
+					bot.reply_to(message,str(eval(exp)))
+			
+			except Exception as e:
+				
+				print(str(e))
+
+				#raise e
+
+		analysis = TextBlob(trans_msg)
+
+		polarity = analysis.sentiment.polarity
+
+		with open('files/polarity.txt','w') as fp:
+
+			fp.write(str(index)+","+str(polarity)+"\n")
+
+		sentiment = 'positive'
+
+		if(polarity==0):
+			
+			sentiment = 'neutral'
+		
+		elif(polarity < -0.5):
+			
+			sentiment = 'Very negative'
+
+			bot.reply_to(message,'Warning !!!!! \n you are using toxic language if you continue this you will get banned .')
+		
+		elif(polarity > 0.5):
+			
+			sentiment = 'Very positive'
+
+		elif(polarity >0 and polarity<=0.5):
+			
+			sentiment = 'positive'
+
+		else:
+			
+			sentiment = 'negative'
+
+		output = "Sentiment : "+sentiment+"\nSentiment Score : % .2f" %(polarity)
+
+		if(sentiment_flag):
+
+			bot.reply_to(message,output)
+
+		print(output)
+
+		Dict = {'neutral':0 , 'positive':1 , 'Very positive':2, 'negative':3 , 'Very negative':4}
+
+		fp = open("files/chat_record.csv","a")
+
+		fp.write("\n"+str(index)+","+name+","+date_time+","+str(Dict[sentiment])+","+sentiment+",% .2f" %(polarity))+","+orig_msg
+
+		fp.close()
+
+	except Exception as e:
+		
+		print(str(e))
+
+		#raise e
+
+bot.polling()
