b77b637a03e9e6cd8ae26101e84cfccbae5a1ceb
Aman Kathait <agnikathait171@gmail.com>
2021-05-09 15:10:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 
 
-bot_token = '#####################################'
+bot_token = '######## enter your bot token ########'
 
-group_id = '######################################'
+group_id = '######### enter your telegram group id #######'
 
 #https://t.me/joinchat/NAf-3Uj4f3xvGHiRlmlEfw
 
